aba3281a0e9f1d47cc1178a8c6555eba8533321b
Mohammed Refat <mohammedrefat53@outlook.com>
2024-09-04 00:27:14 +0300

    task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -43,3 +45,15 @@ class BasicAuth(Auth):
                 ':' not in decoded_base64_authorization_header:
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(':', 1))
+
+    def user_object_from_credentials(self, user_email:
+                                     str, user_pwd: str) -> TypeVar('User'):
+        '''returns the User instance based on his email and password.'''
+        if user_email is None or not isinstance(user_email, str) or \
+           user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        if not User.search({'email': user_email}):
+            return
+        if not self.is_valid_password(user_pwd):
+            return
+        return User.search({'email': user_email})
