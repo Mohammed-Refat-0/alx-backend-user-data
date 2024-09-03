2af18f03f3587c3d1e8c0907ad1eedccd6764253
Mohammed Refat <mohammedrefat53@outlook.com>
2024-09-04 00:06:20 +0300

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """Basic Auth module"""
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,16 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        '''returns the decoded value of a Base64 string
+        base64_authorization_header'''
+        if base64_authorization_header is None or \
+           not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header)\
+                .decode('utf-8')
+        except Exception:
+            return None
