3c638c83729c7f7729b2153f00c754ef718fb808
Mohammed Refat <mohammedrefat53@outlook.com>
2024-09-05 23:18:06 +0300

    task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -55,15 +55,14 @@ def authenticate_user():
             '/api/v1/forbidden/',
             '/api/v1/auth_session/login/',
         ]
-        if auth.authorization_header(request) == None and auth.session_cookie(request) == None:
-            abort(401)
         if auth.require_auth(request.path, excluded_paths):
-            auth_header = auth.authorization_header(request)
-            request.current_user = auth.current_user(request)
-            if auth_header is None:
-                abort(401)
-            if user is None:
-                abort(403)
+            user = auth.current_user(request)
+        if auth.authorization_header(request) is None and \
+                auth.session_cookie(request) is None:
+            abort(401)
+        if user is None:
+            abort(403)
+        request.current_user = user
 
 
 if __name__ == "__main__":
