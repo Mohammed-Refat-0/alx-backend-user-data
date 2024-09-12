6af354bc280883c3e74781ac59232757307493f4
Mohammed Refat <mohammedrefat53@outlook.com>
2024-09-12 05:30:01 +0300

    task 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -31,3 +31,17 @@ class Auth:
             hashpsw = _hash_password(password)
             return self._db.add_user(email, hashpsw)
         raise ValueError("User {} already exists".format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        '''return True if email exists an password matches'''
+        user = None
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                return bcrypt.checkpw(
+                    password.encode("utf-8"),
+                    user.hashed_password,
+                )
+        except NoResultFound:
+            return False
+        return False
