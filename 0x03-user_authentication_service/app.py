93bdb03ad5887011c7fe890b703dea65b708c332
Mohammed Refat <mohammedrefat53@outlook.com>
2024-09-13 01:19:30 +0300

    task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -29,5 +29,20 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    '''login and create a new session'''
+    email, password = request.form.get("email"), request.form.get("password")
+    try:
+        if AUTH.valid_login(email, password):
+            session_id = AUTH.create_session(email)
+            if session_id:
+                response = jsonify({"email": email, "message": "logged in"})
+                response.set_cookie("session_id", session_id)
+                return response
+    except ValueError:
+        Flask.abort(401)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
